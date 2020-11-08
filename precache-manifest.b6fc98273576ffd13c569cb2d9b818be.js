e3a208f92b9010106ebd109e56f4f5f82a0066f3
Helen <elijahsuzzivan@gmail.l.com>
2020-11-08 23:51:52 +0400

    Updates

---
+++ precache-manifest.b6fc98273576ffd13c569cb2d9b818be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d9cf04602fc52ee43738b1b7807bf6f",
+    "url": "/book-store/index.html"
+  },
+  {
+    "revision": "85262ded5dbfaf21a6f3",
+    "url": "/book-store/static/css/main.f9035cc6.chunk.css"
+  },
+  {
+    "revision": "9a77f4e108270a64cd1b",
+    "url": "/book-store/static/js/2.08914367.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/book-store/static/js/2.08914367.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85262ded5dbfaf21a6f3",
+    "url": "/book-store/static/js/main.bf2127ea.chunk.js"
+  },
+  {
+    "revision": "c50a113925d0cdbdf923",
+    "url": "/book-store/static/js/runtime-main.014a42aa.js"
+  }
+]);
